ddc67530c96c38c7b95dea8ce53e2f92b60724ec
acunaj29 <joseacuna2010@gmail.com>
2023-04-11 16:11:30 -0600

    Registro .ignore delete password gmail

--- ProyectoWeb/settings.py
+++ ProyectoWeb/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+from ProyectoWeb.credenciales import credenciales
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -132,9 +132,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #Configuracion de email.
-EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST="smtp.gmail.com"
-EMAIL_USE_TLS=True
-EMAIL_PORT=587
-EMAIL_HOST_USER="joseacuna2010@gmail.com"
-EMAIL_HOST_PASSWORD="wejfnlweiufhqp903"
+credenciales()
