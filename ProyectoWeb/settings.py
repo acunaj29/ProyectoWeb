48aa6ad17fab40b6e70ab0f68e8111e9e60da3af
acunaj29 <joseacuna2010@gmail.com>
2023-04-17 21:51:45 -0600

    add form authentication & crispy_forms

--- ProyectoWeb/settings.py
+++ ProyectoWeb/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'tienda',
     'carro',
     'autenticacion',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -134,12 +135,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #Configuracion de email.
-def credenciales():
-    #Configuracion de email.
-    EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
-    EMAIL_HOST="smtp.gmail.com"
-    EMAIL_USE_TLS=True
-    EMAIL_PORT=587
-    EMAIL_HOST_USER="joseacuna2010@gmail.com"
-    EMAIL_HOST_PASSWORD="wejfnlweiufhqp903"
-#credenciales()
+
+credenciales()
+
+CRISPY_TEMPLATE_PACK='bootstrap4'
